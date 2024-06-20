9fe0c81030df2e2972b7f7a81054f4757f833033
Muhammad-Shah <msh87376@gmail.com>
2024-06-20 21:00:57 +0500

    docs no

--- ingest.py
+++ ingest.py
@@ -6,31 +6,37 @@ from langchain_chroma import Chroma
 import streamlit as st
 import os
 
+
 class Document(BaseModel):
     page_content: str
     metadata: Dict
+
 
 @st.cache_resource
 def load_embeddings_model():
     return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
+
 @st.cache_resource
 def load_documents():
     return json.load(open("data.json", "r"))
+
 
 @st.cache_resource
 def load_embeddings():
     embeddings_model = load_embeddings_model()
     data = load_documents()
     docs = [
-        Document(page_content=str(doc), metadata={'source': '/content/data.json', 'seq_num': index+1})
+        Document(page_content=str(doc), metadata={
+                 'source': '/content/data.json', 'seq_num': index+1})
         for index, doc in enumerate(data)
     ]
     return embeddings_model, docs
 
+
 def create_embeddings():
     persist_directory = './doctors_db'
-    
+
     # Check if the vector store already exists on disk
     if os.path.exists(os.path.join(persist_directory, "./doctors_db")):
         print('It shold run...........')
@@ -43,8 +49,8 @@ def create_embeddings():
             embedding=embeddings_model,
             persist_directory=persist_directory
         )
-    
-    return st.session_state.db.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+
+    return st.session_state.db.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
 # def main():
 #     create_embeddings()
